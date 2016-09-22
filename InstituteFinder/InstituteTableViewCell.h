4c7e268944487016077a4353059ea1ad09d2c798
harinath <harinath@harinaths-mac-mini.local>
2016-09-22 18:54:41 +0530

    coredata

--- InstituteFinder/InstituteTableViewCell.h
+++ InstituteFinder/InstituteTableViewCell.h
@@ -9,12 +9,14 @@
 #import <UIKit/UIKit.h>
 #import "Institute.h"
 #import "InstituteViewController.h"
+#import "IFInsitute.h"
 
 @interface InstituteTableViewCell : UITableViewCell
 
 @property (assign, nonatomic) id <InstituteViewControllerProtocol> instituteViewDelegate;
 
 @property (strong, nonatomic) Institute *institute;
+@property (strong, nonatomic) IFInsitute *iFInsitute;
 
 - (void)updateCellAtIndexPath:(NSIndexPath *)indexPath;
 
